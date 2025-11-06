e1ad79f44a60a8eb9922a4719e412d2594947967
arpittiwari22 <at4072102@gmail.com>
2025-11-06 17:46:24 +0000

    Commit

---
+++ week6/myapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './SimpleCalculator';
+
+function App() {
+  return (
+    <Calculator/>
+    
+  );
+}
+
+export default App;
